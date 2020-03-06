9cd01261a0946c93afa9573a89f6280ec80b6b91
constraint-p <61328459+constraint-p@users.noreply.github.com>
2020-03-06 22:10:56 +0100

    removed chat thingy

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,18 +1,11 @@
 <svelte:head>
 	<title>About</title>
-	<script src="https://api.underpolished.com/js/chatwidget.js" async type="text/javascript"></script>
 </svelte:head>
 
 <h1>About this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>Empty about page for the time being</p>
 
 <script>
-	import { onMount } from 'svelte';
-	onMount(async () => {
-		window.UnderpolishedChatSettings = {
-			id: 10,
-			token: 'oQXfa275QFb9dEyL'
-		};
-	});
+
 </script>
