6c555b17964bb5430447a6dce8a48a6727464afc
constraint-p <61328459+constraint-p@users.noreply.github.com>
2020-03-31 23:21:57 +0200

    move pennyless to page

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<style>
+    h1, figure, p {
+        text-align: center;
+        margin: 0 auto;
+    }
+
+    h1 {
+        font-size: 2.8em;
+        text-transform: uppercase;
+        font-weight: 700;
+        margin: 0 0 0.5em 0;
+    }
+
+    p {
+        margin: 1em auto;
+    }
+
+</style>
+
+<svelte:head>
+    <title>Sapper project template</title>
+</svelte:head>
+
+<h1>Map</h1>
+
