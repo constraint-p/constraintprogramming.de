59a40e8784b6c9e3538c10c9d53eba89d73cfdf5
constraint-p <61328459+constraint-p@users.noreply.github.com>
2020-04-20 15:48:30 +0200

    removing unused styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,11 @@
 <style>
-    h1, figure, p {
-        text-align: center;
-        margin: 0 auto;
-    }
 
     h1 {
+        text-align: center;
         font-size: 2.8em;
         text-transform: uppercase;
         font-weight: 700;
         margin: 0 0 0.5em 0;
-    }
-
-    p {
-        margin: 1em auto;
     }
 
 </style>
